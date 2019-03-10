e4bf3cfc68630639ad93091d0fb70d124ae562b1
dhairyadwivedi <dhairiya96@gmail.com>
2019-03-10 21:01:18 +0530

    Updates

---
+++ precache-manifest.e03d24bc428d252b588ac7c490db27d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "0feb9af6f9635ae46b98",
+    "url": "/book-finder/static/js/main.0feb9af6.chunk.js"
+  },
+  {
+    "revision": "3d4ef649e4f9f784e3b1",
+    "url": "/book-finder/static/js/2.3d4ef649.chunk.js"
+  },
+  {
+    "revision": "0feb9af6f9635ae46b98",
+    "url": "/book-finder/static/css/main.f74fd998.chunk.css"
+  },
+  {
+    "revision": "f2065d1d0dea090a34c55cd1673be3dd",
+    "url": "/book-finder/index.html"
+  }
+];
